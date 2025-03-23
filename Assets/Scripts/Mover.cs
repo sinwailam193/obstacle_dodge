5903cfa05d7434f6fdcb07097ea4ce459d74743d
sinwailam193 <8454621+sinwailam193@users.noreply.github.com>
2025-03-23 20:42:18 +0800

    Added how to move using Input

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,20 +1,17 @@
 using UnityEngine;
 
-public class Mover : MonoBehaviour
-{
-    float xValue = 0.01f;
-    float yValue = 0.01f;
-    float zValue = 0.01f;
-
+public class Mover : MonoBehaviour {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = 0;
+        float zValue = Input.GetAxis("Vertical");
+
         transform.Translate(xValue, yValue, zValue);
     }
 }
